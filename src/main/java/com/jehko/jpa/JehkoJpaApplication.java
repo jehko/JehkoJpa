73920eb5ef9960f8f63ea74e5a739680e8c5489b
Jehko <krelude.jko@gmail.com>
2022-04-06 23:33:38 +0900

    rename Project Name JPASample -> JehkoJpa

--- src/main/java/com/jehko/jpa/JehkoJpaApplication.java
+++ src/main/java/com/jehko/jpa/JehkoJpaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JPASampleApplication {
+public class JehkoJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JPASampleApplication.class, args);
+		SpringApplication.run(JehkoJpaApplication.class, args);
 	}
 
 }
